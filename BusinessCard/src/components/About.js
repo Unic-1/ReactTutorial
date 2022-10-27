8b294773ff3d6f61b22a4781fb471b8a65c8b0a9
satyam <satyam123.sr@gmail.com>
2022-10-27 10:30:58 +0530

    - About component basic structure

--- BusinessCard/src/components/About.js
+++ BusinessCard/src/components/About.js
@@ -1,3 +1,22 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <div className="container about-container">
+      <div className="about-item">
+        <h3>About</h3>
+        <p>
+          Do mollit nisi ad elit irure ea voluptate quis fugiat consectetur
+          aliquip. Amet proident occaecat consequat est enim ut. Ipsum veniam
+          cillum laboris esse sit in sunt in minim magna nulla do.
+        </p>
+      </div>
+      <div className="about-item">
+        <h3>Interests</h3>
+        <p>
+          Do mollit nisi ad elit irure ea voluptate quis fugiat consectetur
+          aliquip. Amet proident occaecat consequat est enim ut. Ipsum veniam
+          cillum laboris esse sit in sunt in minim magna nulla do.
+        </p>
+      </div>
+    </div>
+  );
 }
