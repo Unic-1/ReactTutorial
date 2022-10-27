30008714dd14452412523d26116a9e6e61e4cda1
satyam <satyam123.sr@gmail.com>
2022-10-27 10:39:45 +0530

    - social component basic setup

--- BusinessCard/src/components/Social.js
+++ BusinessCard/src/components/Social.js
@@ -1,3 +1,33 @@
 export default function Social() {
-  return <h1>Social</h1>;
+  return (
+    <footer>
+      <ul className="social-list">
+        <li>
+          <a href="#">
+            <i class="fa-brands fa-square-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa-brands fa-square-facebook"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa-brands fa-square-instagram"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa-brands fa-square-github"></i>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
