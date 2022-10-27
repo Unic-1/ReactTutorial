f4cadbeaf329f4f74d7f169fc3be871deb24c87b
satyam <satyam123.sr@gmail.com>
2022-10-27 10:25:34 +0530

    - Profile component basic structure

--- BusinessCard/src/components/Profile.js
+++ BusinessCard/src/components/Profile.js
@@ -1,3 +1,18 @@
 export default function Profile() {
-  return <h1>Profile</h1>;
+  return (
+    <div className="profile-container">
+      <img src="./static/profile.png" width="100px" />
+      <h2>Sherlock Holmes</h2>
+      <h4>Professional Detective</h4>
+      <a href="#">sherlock.holmes.uk</a>
+      <div className="actions">
+        <button className="btn email">
+          <i class="fa-regular fa-envelope"></i>Email
+        </button>
+        <button className="btn linkedin">
+          <i class="fa-brands fa-linkedin-in"></i>LinkedIn
+        </button>
+      </div>
+    </div>
+  );
 }
