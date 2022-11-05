86ce7af93addc752f865cea37dc12259635d687f
satyam <satyam123.sr@gmail.com>
2022-11-05 15:29:46 +0530

    - Header added

---
+++ TravelJournal/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <span>
+        <a href="#">
+          <span class="material-symbols-outlined">public</span>my travel
+          journal.
+        </a>
+      </span>
+    </header>
+  );
+}
