a75c001f042b7d2830178d0d56beefc9f1c5fb3e
satyam <satyam123.sr@gmail.com>
2022-11-06 22:06:40 +0530

    - empty boxes added

--- Boxes/App.js
+++ Boxes/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import boxes from "./boxes";
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const [allBoxes, setAllBoxes] = React.useState(boxes);
+
+  const boxesComponents = allBoxes.map((box) => (
+    <div className="box" key={box.id}></div>
+  ));
+
+  return (
+    <main>
+      <h1>Boxes will go here</h1>
+      {boxesComponents}
+    </main>
+  );
 }
