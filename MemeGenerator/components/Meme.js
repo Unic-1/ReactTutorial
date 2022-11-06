5737c3cad9ab629258901b6ecd18e96a24878514
satyam <satyam123.sr@gmail.com>
2022-11-06 21:52:51 +0530

    - meme form ready and can now display image

--- MemeGenerator/components/Meme.js
+++ MemeGenerator/components/Meme.js
@@ -1,11 +1,24 @@
 import React from "react";
+import memeData from "../memeData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getRandomMeme() {
+    let index = parseInt(Math.random() * 100) - 1;
+    setMemeImage(memeData.data.memes[index].url);
+  }
+
   return (
-    <form className="meme--form">
-      <input type="text" placeholder="Shut up" />
-      <input type="text" placeholder="and take my money" />
-      <button className="btn">Get a new meme image </button>
-    </form>
+    <main>
+      <div className="meme--form">
+        <input type="text" placeholder="Shut up" />
+        <input type="text" placeholder="and take my money" />
+        <button className="btn" onClick={getRandomMeme}>
+          Get a new meme image{" "}
+        </button>
+      </div>
+      <img src={memeImage} className="meme--image" />
+    </main>
   );
 }
