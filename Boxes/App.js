f55671c4285100ffdbce13a563c14ad348409708
satyam <satyam123.sr@gmail.com>
2022-11-06 22:13:21 +0530

    - Box moved to a component

--- Boxes/App.js
+++ Boxes/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import boxes from "./boxes";
+import Box from "./components/Box";
 
 export default function App() {
   const [allBoxes, setAllBoxes] = React.useState(boxes);
 
   const boxesComponents = allBoxes.map((box) => (
-    <div className="box" key={box.id}></div>
+    <Box on={box.on} key={box.id} />
   ));
 
   return (
